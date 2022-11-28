2d45c6263d82b8060c7500626cb036c1ac4470f0
asok1 <sokolovalex4@gmail.com>
2022-11-28 17:31:00 -0600

    Initial Commit

---
+++ footy Watch App/footyApp.swift
@@ -0,0 +1,17 @@
+//
+//  footyApp.swift
+//  footy Watch App
+//
+//  Created by Alex Sokolov on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct footy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ footy/footyApp.swift
@@ -0,0 +1,17 @@
+//
+//  footyApp.swift
+//  footy
+//
+//  Created by Alex Sokolov on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct footyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
